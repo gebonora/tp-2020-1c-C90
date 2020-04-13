a589ba70979247eeaa71474302c15a84c007cdff
gebonora <gebonora@gmail.com>
2020-04-13 19:59:46 -0300

    Subo makefile y un main

---
+++ gamecard/include/main.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: 13 abr. 2020
+ *      Author: GONZALO BONORA
+ */
+
+#ifndef GAMECARD_INCLUDE_MAIN_H_
+#define GAMECARD_INCLUDE_MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#endif /* GAMECARD_INCLUDE_MAIN_H_ */
