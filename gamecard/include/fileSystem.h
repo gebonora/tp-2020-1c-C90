5dc0ddae90661df79bb072991129e0382254514e
gebonora <gebonora@gmail.com>
2020-04-19 23:51:15 -0300

    Meditaciones.

---
+++ gamecard/include/fileSystem.h
@@ -0,0 +1,13 @@
+/*
+ * fileSystem.h
+ *
+ *  Created on: 19 abr. 2020
+ *      Author: GONZALO BONORA
+ */
+
+#ifndef GAMECARD_INCLUDE_FILESYSTEM_H_
+#define GAMECARD_INCLUDE_FILESYSTEM_H_
+
+
+
+#endif /* GAMECARD_INCLUDE_FILESYSTEM_H_ */
---
+++ gamecard/src/fileSystem.c
@@ -0,0 +1,23 @@
+/*
+ * fileSystem.c
+ *
+ *  Created on: 19 abr. 2020
+ *      Author: GONZALO BONORA
+ */
+
+/* Paso inicial: Crear la estructura del Filesystem:
+ * 					- Leer configs.
+ * 					- Crear directorios necesarios para metadata .
+ * 					- Implementar Logs.
+ *
+ * Segundo paso: Iniciar desarrollo de funciones del FS:
+ * 					- Logica de creacion de directorios y guardado de informacion en metadata.
+ * 					- Crear archivos pokemon vacios.
+ *
+ */
+
+
+/* Dudas:
+ *  1- El fs es single thread? Pareceria que si...
+ *  2- Es un hilo para "Server Broker" y otro para "Server socket gameboy"?
+ */
