9e7d06aeae84af837374b4978e5fcf79f4c75a25
Alan Zhao <alanzhag@hotmail.com>
2020-04-22 21:34:32 -0300

    Commit inicial. Base de GameBoy.

---
+++ GameBoy/src/App.c
@@ -0,0 +1,33 @@
+#include "app/App.h"
+
+int main() {
+    t_log *logger = log_create("logs/gameboy.log", "GameBoy.app", 1, LOG_LEVEL_INFO);
+    mostrarTitulo(logger);
+    log_info(logger, "========================= Inicio de ejecución ============================");
+
+    // Config - TODO: Migrar a servicio
+    log_debug(logger, "Levantando configuracion...");
+    t_config *config = config_create(GAMEBOY_CONFIG_FILE);
+    char *clave = "PUERTO_TEAM";
+    char *valor = config_get_string_value(config, clave);
+    log_info(logger, "Se obtuvo un string de la configuracion: %s -> %s", clave, valor);
+
+    //Liberacion
+    log_debug(logger, "Saliendo...");
+    log_info(logger, "============================ Fin de ejecución ============================");
+    log_destroy(logger);
+    return 0;
+}
+
+void mostrarTitulo(t_log * logger) {
+    char *title = "\n"
+                  "==============================================================\n"
+                  " ██████   █████  ███    ███ ███████ ██████   ██████  ██    ██ \n"
+                  "██       ██   ██ ████  ████ ██      ██   ██ ██    ██  ██  ██  \n"
+                  "██   ███ ███████ ██ ████ ██ █████   ██████  ██    ██   ████   \n"
+                  "██    ██ ██   ██ ██  ██  ██ ██      ██   ██ ██    ██    ██    \n"
+                  " ██████  ██   ██ ██      ██ ███████ ██████   ██████     ██    \n"
+                  "==============================================================";
+
+    log_info(logger, title);
+}
