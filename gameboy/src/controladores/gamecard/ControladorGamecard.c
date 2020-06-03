408b94f8cdec2cf0fbd7950b1514ea3b46d33eb4
Gonzalo <gebonora@gmail.com>
2020-06-02 23:50:11 -0300

    subo parte de gamecard para gameboy. To do: definir que hacer con id mensajes.

--- gameboy/src/controladores/gamecard/ControladorGamecard.c
+++ gameboy/src/controladores/gamecard/ControladorGamecard.c
@@ -12,21 +12,53 @@ void atenderPedidoGamecard(PedidoGameBoy pedidoGameBoy, t_log * logger) {
 	char* puerto = servicioDeConfiguracion.obtenerString(
 			&servicioDeConfiguracion, PUERTO_GAMECARD);
 	int socketCliente = crearSocketCliente(IP_GAMECARD, PUERTO_GAMECARD);
-
+	uint32_t idMensaje;
 	switch (pedidoGameBoy.tipoMensaje) {
 	case NEW_POKEMON:
+		;
+		//[POKEMON] [POSX] [POSY] [CANTIDAD] [ID_MENSAJE]
+		New* newPokemon = create_new_pokemon(
+				list_get(pedidoGameBoy.argumentos, 0),
+				atoi(list_get(pedidoGameBoy.argumentos, 1)),
+				atoi(list_get(pedidoGameBoy.argumentos, 2)),
+				atoi(list_get(pedidoGameBoy.argumentos, 3)));
+		send_new(newPokemon, socketCliente);
+		idMensaje = atoi(list_get(pedidoGameBoy.argumentos, 4));
+		send(socketCliente, &idMensaje, sizeof(uint32_t), MSG_WAITALL);
+
 		//implementar
 		break;
 
 	case CATCH_POKEMON:
-		//implementar
+		;
+		//[POKEMON] [POSX] [POSY] [ID_MENSAJE]
+		Pokemon* catch_pokemon = create_pokemon(
+				list_get(pedidoGameBoy.argumentos, 0),
+				atoi(list_get(pedidoGameBoy.argumentos, 1)),
+				atoi(list_get(pedidoGameBoy.argumentos, 2)));
+		send_pokemon(catch_pokemon, CATCH, socketCliente);
+		idMensaje = atoi(list_get(pedidoGameBoy.argumentos, 3));
+		send(socketCliente, &idMensaje, sizeof(uint32_t), MSG_WAITALL);
+
 		break;
 
 	case GET_POKEMON:
-		//implementar
+		;
+		//[POKEMON] [ID_MENSAJE]
+		Get* get_pokemon = create_get_pokemon(
+				list_get(pedidoGameBoy.argumentos, 0));
+		send_get(get_pokemon, socketCliente);
+		idMensaje = atoi(list_get(pedidoGameBoy.argumentos, 1));
+		send(socketCliente, &idMensaje, sizeof(uint32_t), MSG_WAITALL);
+
 		break;
 
 	}
+	/* ToDo: ver el tema de mensaje id, definir si se lo manda aparte de las structs
+	 * o adentro. Ver de abstraer logica para enviar los id, serian enviar un int capaz conviene usar uint32_t
+	 * por el tema de eventual compatibilidad (igual que los demas enteros del protocolo).
+	 */
+
 
 }
 
