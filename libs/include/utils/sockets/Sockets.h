4b728fb140efcd7ccd5b147bd445d64837814e14
Alan Zhao <alanzhag@hotmail.com>
2020-06-05 03:23:43 -0300

    Libs renombra a delibird ifndef por la muerte de support. Cambio inocuo.

--- libs/include/utils/sockets/Sockets.h
+++ libs/include/utils/sockets/Sockets.h
@@ -5,8 +5,8 @@
  *      Author: utnso
  */
 
-#ifndef SUPPORT_INCLUDE_UTILS_SOCKETS_SOCKETS_H_
-#define SUPPORT_INCLUDE_UTILS_SOCKETS_SOCKETS_H_
+#ifndef DELIBIRD_INCLUDE_UTILS_SOCKETS_SOCKETS_H_
+#define DELIBIRD_INCLUDE_UTILS_SOCKETS_SOCKETS_H_
 
 #include <sys/socket.h>
 #include <netdb.h>
@@ -17,4 +17,4 @@
 int crearSocketCliente(char* ip, char* puerto);
 int crearSocketServidor(char* ip, char* puerto);
 
-#endif /* SUPPORT_INCLUDE_UTILS_SOCKETS_SOCKETS_H_ */
+#endif /* DELIBIRD_INCLUDE_UTILS_SOCKETS_SOCKETS_H_ */
