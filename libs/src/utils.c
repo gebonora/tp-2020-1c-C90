9f5f2fa906645b29770790cbd4de684f1b33112f
Gonzalo <gebonora@gmail.com>
2020-06-14 14:49:04 -0300

    utils.c faltante

--- libs/src/utils.c
+++ libs/src/utils.c
@@ -1,25 +1,51 @@
 #include "../include/interface.h"
 
-uint32_t recv_uint32(int socket_e) {
+uint32_t recv_uint32(int socket_e) { //changed
 	uint32_t recibido;
-	recv(socket_e, &recibido, sizeof(uint32_t), 0);
+	if (recv(socket_e, &recibido, sizeof(uint32_t), 0) <= 0) {
+		return -1;
+	}
 	return recibido;
 }
 
-Name* recv_name(int socket_e) {
+Name* recv_name(int socket_e) { //changed cuando falla una funcion que recibe, hace free -> se devuelve puntero a NULL.
 	Name* name = malloc(sizeof(Name));
 	uint32_t name_size = recv_uint32(socket_e);
+
+	if (name_size < 0) {
+		free(name);
+		return NULL;
+	}
+
 	char* name_value = malloc(name_size);
-	recv(socket_e, name_value, name_size, MSG_WAITALL);
+
+	if (recv(socket_e, name_value, name_size, MSG_WAITALL) <= 0) {
+		free(name);
+		free(name_value);
+		return NULL;
+	}
+
 	name->size = name_size;
 	name->value = name_value;
 	return name;
 }
 
-Coordinate* recv_coordinate(int socket_e) {
+Coordinate* recv_coordinate(int socket_e) { //changed
 	Coordinate* coordinate = malloc(sizeof(Coordinate));
 	uint32_t pos_x = recv_uint32(socket_e);
+
+	if (pos_x < 0) {
+		free(coordinate);
+		return NULL;
+	}
+
 	uint32_t pos_y = recv_uint32(socket_e);
+
+	if (pos_y < 0) {
+		free(coordinate);
+		return NULL;
+	}
+
 	coordinate->pos_x = pos_x;
 	coordinate->pos_y = pos_y;
 	return coordinate;
@@ -32,20 +58,43 @@ Coordinate* create_coordinate(uint32_t posx, uint32_t posy) {
 	return coor;
 }
 
-t_list* recv_coordinates(int socket_e, bool multiple_coordinates) {
+t_list* recv_coordinates(int socket_e, bool multiple_coordinates) { //changed
 	t_list* coordinates = list_create();
-	if(multiple_coordinates) {
+	if (multiple_coordinates) {
 		uint32_t number_of_coordinates = recv_uint32(socket_e);
 
-		for(int i = 0; i < number_of_coordinates; i++) {
-			list_add(coordinates, recv_coordinate(socket_e));
+		if (number_of_coordinates < 0) {
+			free(coordinates);
+			return NULL;
+		}
+
+		for (int i = 0; i < number_of_coordinates; i++) {
+			Coordinate* coor = recv_coordinate(socket_e);
+
+			if (coor == NULL) {
+				//free elemenots dde la lista: sirve esto? NOTA: usar siempre destroy_and_elemnts??
+				list_destroy_and_destroy_elements(coordinates,&free_coordinate );
+				return NULL;
+			}
+
+			list_add(coordinates, coor);
 		}
 
 	} else {
 		Coordinate* coor = recv_coordinate(socket_e);
+
+		if (coor == NULL) {
+			free(coordinates);
+			return NULL;
+		}
+
 		list_add(coordinates, coor);
 	}
 	return coordinates;
+}
+
+void free_coordinate(void* coor) {
+	free((Coordinate*)coor);
 }
 
 void free_name(Name* name) {
@@ -78,7 +127,7 @@ int create_connection(char *ip, char* puerto) {
 
 	int socket_cliente = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
 
-	if(connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen) == -1)
+	if (connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen) == -1)
 		printf("error");
 
 	freeaddrinfo(server_info);
@@ -86,26 +135,33 @@ int create_connection(char *ip, char* puerto) {
 	return socket_cliente;
 }
 
-char* get_operation_by_value(int value){
-	switch(value){
-	case 1: ;
+char* get_operation_by_value(int value) {
+	switch (value) {
+	case 1:
+		;
 		return "NEW";
 		break;
-	case 2: ;
+	case 2:
+		;
 		return "LOCALIZED";
 		break;
-	case 3: ;
+	case 3:
+		;
 		return "GET";
 		break;
-	case 4: ;
+	case 4:
+		;
 		return "APPEARED";
 		break;
-	case 5: ;
+	case 5:
+		;
 		return "CATCH";
 		break;
-	case 6: ;
+	case 6:
+		;
 		return "CAUGHT";
 		break;
-	default: return "SUBSCRIBE";
+	default:
+		return "SUBSCRIBE";
 	}
 }
