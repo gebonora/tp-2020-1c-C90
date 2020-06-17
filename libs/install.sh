f8600a4797f8039b69fa25f9bb0256d76aae408f
Alan Zhao <alanzhag@hotmail.com>
2020-06-17 20:52:47 -0300

    Se agrega camino infeliz

--- libs/install.sh
+++ libs/install.sh
@@ -3,7 +3,7 @@
 RELATIVE_MODE="-r"
 LIBS_PATH=/home/utnso/tp-2020-1c-C90/libs
 
-if [ "${RELATIVE_MODE}" == "$1" ]; then
+if [ "${RELATIVE_MODE}" = "$1" ]; then
     LIBS_PATH=`pwd`
 fi
 
@@ -14,11 +14,19 @@ echo "install.sh libs path: " ${LIBS_PATH}
 echo "install.sh deploy path: " ${DEPLOY_PATH}
 
 GREEN='\033[0;32m'
+RED='\033[0;31m'
 NC='\033[0m'
 
 mkdir -p ${DEPLOY_PATH}
 rm -rf ${DEPLOY_PATH}/*
 cd ${DEPLOY_PATH} && cmake .. && sudo make install
+install_result=$?
 cp ${DEPLOY_PATH}/${MANIFEST} ${LIBS_PATH}/${MANIFEST}
 rm -rf ${DEPLOY_PATH}
-echo -e "${GREEN}Libreria compartida -Delibird- instalada!${NC}"
+
+if [ $install_result -eq 0 ]
+then
+  echo -e "${GREEN}Libreria compartida -Delibird- instalada!${NC}"
+else
+  echo -e "${RED}Error al instalar libreria compartida${NC}"
+fi
