3e8f0023205f8e35f613d721ec4ce3e896f009bf
Sebastian Gilberto <sebastian.gilberto@despegar.com>
2020-06-17 20:54:03 -0300

    Cambio que me quedo local

--- delibird.sh
+++ delibird.sh
@@ -12,8 +12,8 @@ echo "delibird.sh project path: " ${DELIBIRD_PATH}
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-cd ${DELIBIRD_PATH}/libs && ./install.sh
-cd ${DELIBIRD_PATH}/gameboy && ./deploy.sh
+cd ${DELIBIRD_PATH}/libs && ./install.sh $1
+cd ${DELIBIRD_PATH}/gameboy && ./deploy.sh $1
 cd ${DELIBIRD_PATH}/broker && make
 #cd ${DELIBIRD_PATH}/gamecard && make
 #cd ${DELIBIRD_PATH}/team && make
