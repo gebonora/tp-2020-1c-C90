ecd717cc01675f1f2648c5e10197115292dbf310
Alan Zhao <alanzhag@hotmail.com>
2020-06-18 22:19:51 -0300

    Se descomenta el atender conexiones aunque trabe el proceso.

--- team/src/App.c
+++ team/src/App.c
@@ -31,7 +31,7 @@ int main() {
     // Server
     log_info(INTERNAL_LOGGER, "Levantando el server...");
     configurarServer(); //TODO: Usar como fuente de datos el servicio de config
-    //atenderConexiones();
+    atenderConexiones(); //TODO: Hacer apagable
 
     // Por cada pokemon del objetivo global, enviar un GET [POKEMON].
     log_info(INTERNAL_LOGGER, "Solicitando la ubicacion de los pokemones objetivo para comenzar...");
