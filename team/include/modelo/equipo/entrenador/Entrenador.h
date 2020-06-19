4b2f6b8e3af4886720bdc1c3aff43395c0c4cfbe
Alan Zhao <alanzhag@hotmail.com>
2020-06-19 03:24:39 -0300

    Se agregan mas atributos al entrenador.

--- team/include/modelo/equipo/entrenador/Entrenador.h
+++ team/include/modelo/equipo/entrenador/Entrenador.h
@@ -9,6 +9,8 @@
 #include "app/Global.h"
 #include "delibird/servicios/servicioDeConfiguracion/ServicioDeConfiguracion.h"
 #include "delibird/interface.h"
+#include "delibird/utils/memoria/Memoria.h"
+#include "delibird/utils/listas/Listas.h"
 
 /**
  * El entrenador se mueve y caza pokemones. La idea es llevar ese registro acá.
@@ -19,9 +21,9 @@ typedef t_dictionary * ContadorPokemones; // La key es el nombre del pokemon, el
 typedef struct Entrenador {
     t_log * logger;
     Coordinate posicionInicial;
-    ContadorPokemones pokemones_iniciales;
-    ContadorPokemones pokemones_objetivo;
-    // limiteDeCaptura : int // Establece cuantos pokemones puede capturar
+    ContadorPokemones pokemonesIniciales;
+    ContadorPokemones pokemonesObjetivo;
+    int limiteDeCaptura; // Establece cuantos pokemones puede capturar
 
     // Interfaz publica
     // objetivoCompletado() : Bool - Es true si pokemones_objetivo es igual a pokemones_capturados.
@@ -33,7 +35,9 @@ extern const struct EntrenadorClass {
     Entrenador *(*new)(char * posicionInicial, char * pokemones_iniciales, char * pokemones_objetivos);
 } EntrenadorConstructor;
 
+// Funciones auxiliares
 Coordinate parsearPosicion(char * posicion); // Toma '1|2' y devuelve las coordenadas (1,2)
 ContadorPokemones agruparPokemonesPorNombre(char * nombreDeLosPokemones);
+int totalDePokemones(ContadorPokemones contadorPokemones); // Nos dice cuantos hay en el contador
 
 #endif //TEAM_ENTRENADOR_H
