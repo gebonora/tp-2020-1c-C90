378eb0711ddd18d0377d3deea160a2ac0390c381
Alan Zhao <alanzhag@hotmail.com>
2020-06-19 05:15:02 -0300

    Se agregan metodos al entrenador. Ext. fun de colecciones para libs.

--- team/src/test/TestDeIntegracion.c
+++ team/src/test/TestDeIntegracion.c
@@ -11,11 +11,22 @@ void testDeIntegracion() {
 
     AlgoritmoPlanificador miAlgoritmo = obtenerAlgoritmo("FIFO");
     t_list * listaReady = list_create();
-    Entrenador * entrenador = EntrenadorConstructor.new("1|2", "A|A|B", "A|D|D");
+    Entrenador * entrenador = EntrenadorConstructor.new("1|2", "A|B", "A|D|D");
+    Entrenador * entrenador2 = EntrenadorConstructor.new("2|4", "A|B", "A|B");
+
+    if (entrenador->objetivoCompletado(entrenador) != false || entrenador->puedeAtraparPokemones(entrenador) != true) {
+        exit(EXIT_FAILURE);
+    }
+
+    if (entrenador2->objetivoCompletado(entrenador2) != true || entrenador2->puedeAtraparPokemones(entrenador2) != false) {
+        exit(EXIT_FAILURE);
+    }
+
     HiloEntrenadorPlanificable * hiloEntrenadorPlanificable = HiloEntrenadorPlanificableConstructor.new(entrenador);
     list_add(listaReady, hiloEntrenadorPlanificable);
     miAlgoritmo.proximoAEjecutar(&miAlgoritmo, listaReady);
     miAlgoritmo.destruir(&miAlgoritmo);
     entrenador->destruir(entrenador);
+    entrenador2->destruir(entrenador2);
     list_destroy_and_destroy_elements(listaReady, (void (*)(void *)) hiloEntrenadorPlanificable->destruir);
 }
