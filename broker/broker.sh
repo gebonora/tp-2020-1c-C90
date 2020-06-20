11a0580d7e252cadddee10a19da7819cd3f99b99
Sebastian Gilberto <sebastiananibalgilberto@gmail.com>
2020-06-20 19:08:32 -0300

    WIP script

--- broker/broker.sh
+++ broker/broker.sh
@@ -12,15 +12,15 @@ if [ -z "${PID}" ]; then
     case $1 in
         -v)
             echo -e "${YELLOW}Starting broker in valgrind mode${NC}"
-            #valgrind --show-leak-kinds=all --leak-check=full --log-file="valgrind.log" -v ./broker.app &;;
+            #valgrind --show-leak-kinds=all --leak-check=full --log-file="valgrind.log" -v ./broker.app &
             ;;
         -h)
             echo -e "${YELLOW}Starting broker in helgrind mode${NC}"
-            #valgrind --tool="helgrind" --show-leak-kinds=all --leak-check=full --log-file="helgrind.log" -v ./broker.app &;;
+            #valgrind --tool="helgrind" --show-leak-kinds=all --leak-check=full --log-file="helgrind.log" -v ./broker.app &
             ;;
         *)
             echo -e "${YELLOW}Starting broker${NC}"
-            #./broker.app &
+            ./broker.app &
             ;;
     esac
 
@@ -34,18 +34,19 @@ if [ -z "${PID}" ]; then
 
 elif [ "-k" == "$1" ]; then
 
-    echo -e "${GREEN}Killing broker with PID: ${PID}${NC}"
-    #kill -9 pid
+    echo -e "${YELLOW}Sending SIGTERM signal to broker with PID: ${PID}${NC}"
+    
+    kill -s SIGTERM ${PID}
 
     PID=$(pidof broker.app)
 
     if [ -z "${PID}" ]
     then
-        echo -e "${GREEN}Broker killed${NC}"
+        echo -e "${GREEN}Broker terminated succesfully${NC}"
     else
         echo -e "${RED}Something went wrong${NC}"
     fi
 
 else
-    echo -e "${YELLOW}Broker already running with PID: ${PID}${NC}"
+    echo -e "${YELLOW}Broker running with PID: ${PID}${NC}"
 fi
