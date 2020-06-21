f1e2fad32aa54ccdec1c40d3e412dc4b84784163
Alan Zhao <alanzhag@hotmail.com>
2020-06-21 06:09:14 -0300

    Se agregan funciones de hasheo y random para libs.

---
+++ libs/src/utils/hasheo/Hasheador.c
@@ -0,0 +1,13 @@
+//
+// Created by Alan Zhao on 20/06/2020.
+//
+
+unsigned long hash(unsigned char *str) {
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
+}
