05c38ef2b5961131da155c36e95684d4a7e10543
Alan Zhao <alanzhag@hotmail.com>
2020-06-27 05:44:24 -0300

    Merge mess fixed.

--- team/include/app/App.h
+++ team/include/app/App.h
@@ -14,5 +14,8 @@
 
 void warmUp();
 void mostrarTitulo(t_log * logger);
+void inicializarComponentesDelSistema();
+void configurarEstadoInicialProcesoTeam();
+void liberarRecursos();
 
 #endif //TEAM_APP_H
