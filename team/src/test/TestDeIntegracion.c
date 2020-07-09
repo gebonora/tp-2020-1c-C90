30dac0156aac6f304b3f3207355c7290563e8e14
Alan Zhao <alanzhag@hotmail.com>
2020-07-09 12:21:50 -0300

    Refactor tests WIP

--- team/src/test/TestDeIntegracion.c
+++ team/src/test/TestDeIntegracion.c
@@ -32,57 +32,6 @@ void testDeIntegracion() {
     log_destroy(testLogger);
 }
 
-void testDeEntrenadores() {
-    log_info(testLogger, "Testeando los metodos de los entrenadores");
-    Entrenador * entrenador = EntrenadorConstructor.new("1|2", "A|B", "A|C|C");
-    Entrenador * entrenador2 = EntrenadorConstructor.new("2|4", "A|B", "A|B");
-
-    assert(entrenador->objetivoCompletado(entrenador) == false);
-    assert(entrenador->puedeAtraparPokemones(entrenador) == true);
-    assert(entrenador2->objetivoCompletado(entrenador2) == true);
-    assert(entrenador2->puedeAtraparPokemones(entrenador2) == false);
-    assert((int) dictionary_get(entrenador->pokemonesCapturados, "A") == 1);
-    assert((int) dictionary_get(entrenador->pokemonesCapturados, "B") == 1);
-    assert((int) dictionary_get(entrenador->pokemonesObjetivo, "A") == 1);
-    assert((int) dictionary_get(entrenador->pokemonesObjetivo, "C") == 2);
-    assert((int) dictionary_get(entrenador2->pokemonesCapturados, "A") == 1);
-    assert((int) dictionary_get(entrenador2->pokemonesCapturados, "B") == 1);
-    assert((int) dictionary_get(entrenador2->pokemonesObjetivo, "A") == 1);
-    assert((int) dictionary_get(entrenador2->pokemonesObjetivo, "B") == 1);
-
-    Equipo equipito = list_create();
-    list_add(equipito, entrenador);
-    list_add(equipito, entrenador2);
-
-    log_info(testLogger, "Testeando los metodos del objetivo global");
-    ObjetivoGlobal objetivo = ObjetivoGlobalConstructor.new(equipito);
-
-    t_list * especiesNecesarias = objetivo.especiesNecesarias(&objetivo);
-
-    ContabilidadEspecie * contabilidadEspecieA = dictionary_get(objetivo.contabilidadEspecies, "A");
-    ContabilidadEspecie * contabilidadEspecieB = dictionary_get(objetivo.contabilidadEspecies, "B");
-    ContabilidadEspecie * contabilidadEspecieC = dictionary_get(objetivo.contabilidadEspecies, "C");
-
-    assert(list_size(especiesNecesarias) == 3);
-    assert(string_equals(list_get(especiesNecesarias, 0),"A"));
-    assert(string_equals(list_get(especiesNecesarias, 1),"B"));
-    assert(string_equals(list_get(especiesNecesarias, 2),"C"));
-    assert(objetivo.puedeCapturarse(&objetivo, "A") == false);
-    assert(objetivo.puedeCapturarse(&objetivo, "B") == false);
-    assert(objetivo.puedeCapturarse(&objetivo, "C") == true);
-    assert(objetivo.puedeCapturarse(&objetivo, "X") == false);
-    assert(contabilidadEspecieA->necesarios == 2);
-    assert(contabilidadEspecieA->capturados == 2);
-    assert(contabilidadEspecieB->necesarios == 1);
-    assert(contabilidadEspecieB->capturados == 2);
-    assert(contabilidadEspecieC->necesarios == 2);
-    assert(contabilidadEspecieC->capturados == 0);
-
-    list_destroy(especiesNecesarias);
-    destruirEquipo(equipito);
-    objetivo.destruirObjetivoGlobal(&objetivo);
-}
-
 void testDeMovimiento() {
     log_info(testLogger, "Testeando la correcta generacion de caminos entre coordenadas");
     Coordinate origen = {.pos_x = 1, .pos_y = 3};
