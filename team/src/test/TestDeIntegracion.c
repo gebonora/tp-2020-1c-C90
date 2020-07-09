2fd5c910da51d5df40132d3d73397f15096b0b18
Alan Zhao <alanzhag@hotmail.com>
2020-07-09 11:23:43 -0300

    Se amplia cobertura tests.

--- team/src/test/TestDeIntegracion.c
+++ team/src/test/TestDeIntegracion.c
@@ -210,7 +210,6 @@ void testDePlanificable() {
 
     hiloEntrenadorPlanificable->ejecutar(hiloEntrenadorPlanificable, tareaCaptura);
 
-
     assert(tareaCaptura->estado == FINALIZADA);
 
     log_info(testLogger, "Testeando el efecto de lado de la tarea de captura");
@@ -219,7 +218,23 @@ void testDePlanificable() {
     assert(posicionDeCaptura.coordenada.pos_x == 3);
     assert(posicionDeCaptura.coordenada.pos_y == 5);
 
+    log_info(testLogger, "Testeando la ejecucion parcial de una tarea de captura");
+    Coordinate posicionPokemon2 = (Coordinate){.pos_x=7, .pos_y=3};
+    TareaPlanificable * tareaCapturaParcial = generarTareaDeCaptura(hiloEntrenadorPlanificable->entrenador, "A", posicionPokemon2);
+
+    hiloEntrenadorPlanificable->ejecutarParcialmente(hiloEntrenadorPlanificable, tareaCapturaParcial, 1);
+
+    assert(tareaCapturaParcial->estado == PENDIENTE_DE_EJECUCION);
+
+    log_info(testLogger, "Testeando el efecto de lado de la tarea de captura parcialmente completada");
+
+    Posicion posicionEnCaminoACapturar = entrenador->posicion(entrenador);
+    assert(posicionEnCaminoACapturar.valida == true);
+    assert(posicionEnCaminoACapturar.coordenada.pos_x == 4);
+    assert(posicionEnCaminoACapturar.coordenada.pos_y == 5);
+
     tareaCaptura->destruir(tareaCaptura);
+    tareaCapturaParcial->destruir(tareaCapturaParcial);
     hiloEntrenadorPlanificable->destruir(hiloEntrenadorPlanificable);
     entrenador->destruir(entrenador);
     mapita.destruir(&mapita);
