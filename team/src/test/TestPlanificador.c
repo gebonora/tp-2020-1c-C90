eee3063555f781e67feb5feb86d489b2c5a98c3b
Alan Zhao <alanzhag@hotmail.com>
2020-07-11 17:56:01 -0300

    Test de planificador WIP.

--- team/src/test/TestPlanificador.c
+++ team/src/test/TestPlanificador.c
@@ -10,13 +10,31 @@ void testDePlanificador() {
     log_info(testLogger, "Testeando al planificador");
 
     Entrenador * entrenador1 = EntrenadorConstructor.new("0|0", "A", "B");
-    HiloEntrenadorPlanificable * entrenadorPlanificable = HiloEntrenadorPlanificableConstructor.new(entrenador1);
+    Entrenador * entrenador2 = EntrenadorConstructor.new("0|0", "A", "B");
+    Entrenador * entrenador3 = EntrenadorConstructor.new("0|0", "A", "B");
+    HiloEntrenadorPlanificable * entrenadorPlanificable1 = HiloEntrenadorPlanificableConstructor.new(entrenador1);
+    HiloEntrenadorPlanificable * entrenadorPlanificable2 = HiloEntrenadorPlanificableConstructor.new(entrenador2);
+    HiloEntrenadorPlanificable * entrenadorPlanificable3 = HiloEntrenadorPlanificableConstructor.new(entrenador3);
     Planificador planificador = PlanificadorConstructor.new();
-    planificador.agregarUnidadPlanificable(&planificador, entrenadorPlanificable);
 
     //TODO: Test de planificador
+    log_info(testLogger, "Testeando que una nueva unidad planificable vayan a parar a NEW");
+    planificador.agregarUnidadPlanificable(&planificador, entrenadorPlanificable1);
+    assert(list_size(planificador.colas->colaNew) == 1);
+
+    t_list * planificables = list_create();
+    list_add(planificables, entrenadorPlanificable2);
+    list_add(planificables, entrenadorPlanificable3);
+
+    log_info(testLogger, "Testeando que varias nuevas unidades planificables vayan a parar a NEW");
+    planificador.agregarUnidadesPlanificables(&planificador, planificables);
+    assert(list_size(planificador.colas->colaNew) == 3);
 
     planificador.destruir(&planificador, destruirUnidadPlanificable);
 
+    entrenador1->destruir(entrenador1);
+    entrenador2->destruir(entrenador2);
+    entrenador3->destruir(entrenador3);
+    list_destroy(planificables);
     log_destroy(testLogger);
 }
