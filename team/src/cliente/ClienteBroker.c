71f04394ca95b9d0353f6c54e92b4b055a82651c
Gonzalo <gebonora@gmail.com>
2020-07-12 01:52:12 -0300

    Envio de mensajes a broker en cliente. Falta el enganche con el manejadorDeEventos

--- team/src/cliente/ClienteBroker.c
+++ team/src/cliente/ClienteBroker.c
@@ -1,5 +1,42 @@
 #include "cliente/ClienteBroker.h"
 
+void enviarGet(char* nombrePokemon) {
+	int socketDescartable = crearSocketCliente(IP_Broker, Puerto_Broker);
+	int flagBrokerCaido = 0;
+	Get* unGet = create_get_pokemon(nombrePokemon);
 
+	if (send_get(unGet, socketDescartable)) {
+		flagBrokerCaido = 1;
+	}
+	uint32_t idAsignado;
 
+	if (recv(socketDescartable, &idAsignado, sizeof(uint32_t), 0) <= 0) {
+		flagBrokerCaido = 1;
+	}
 
+	if (flagBrokerCaido) {
+		//comportamiento
+	} else {
+		//comportamiento caso broker caido
+	}
+}
+
+void enviarCatch(char* nombrePokemon, uint32_t posX, uint32_t posY) {
+	int socketDescartable = crearSocketCliente(IP_Broker, Puerto_Broker);
+	int flagBrokerCaido = 0;
+	Pokemon* unPokemon = create_pokemon(nombrePokemon, posX, posY);
+	if (send_pokemon(unPokemon, socketDescartable)) {
+		flagBrokerCaido = 1;
+	}
+	uint32_t idAsignado;
+
+	if (recv(socketDescartable, &idAsignado, sizeof(uint32_t), 0) <= 0) {
+		flagBrokerCaido = 1;
+	}
+
+	if (flagBrokerCaido) {
+		//comportamiento
+	} else {
+		//comportamiento caso broker caido
+	}
+}
