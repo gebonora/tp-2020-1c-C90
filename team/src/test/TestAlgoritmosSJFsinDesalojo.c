77e46bd049f2338a8f6054bfcc4713a7b97f1e2c
Gaston Gonzalez Sapia <ggonzaleasapia@gmail.com>
2020-07-18 02:46:44 -0300

    list_map cambia a list_iterate en SJF-SD

--- team/src/test/TestAlgoritmosSJFsinDesalojo.c
+++ team/src/test/TestAlgoritmosSJFsinDesalojo.c
@@ -8,6 +8,8 @@
 #include "test/TestDeIntegracion.h"
 
 
+//TODO: realizar el test para probar que funcione como queremos que lo haga
+
 void testDeAlgoritmosSJFsinDesalojo() {
     t_log * testLogger = log_create(TEAM_INTERNAL_LOG_FILE, "TestAlgoritmoSJFsinDesalojo", 1, LOG_LEVEL_INFO);
     log_destroy(testLogger);
