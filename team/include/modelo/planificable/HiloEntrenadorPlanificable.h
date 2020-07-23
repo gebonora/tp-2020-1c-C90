ac298b35b276382487b303fbd4ca78cf7443aa71
Gaston Gonzalez Sapia <ggonzaleasapia@gmail.com>
2020-07-23 18:50:13 -0300

    merge team dentro de sjf-team

--- team/include/modelo/planificable/HiloEntrenadorPlanificable.h
+++ team/include/modelo/planificable/HiloEntrenadorPlanificable.h
@@ -30,11 +30,11 @@
  */
 
 typedef struct InfoUltimaEjecucion {
-    double est_raf_ant;
-    double real_raf_ant;
-    double est_raf_actual;
-    bool seEjecutoPrimeraEstimacion;
-    double rafaga_real_actual;
+    double est_raf_ant; // estimado anterior
+    double real_raf_ant;// rafaga real anterior
+    double est_raf_actual;// estimado actual
+    bool seEjecutoPrimeraEstimacion; // para saber si hay que usar la estimacion inicial del config
+    double rafaga_real_actual; // rafaga real actual
 
     // parametros para sjf con desalojo}
     bool seNecesitaNuevaEstimacion;
