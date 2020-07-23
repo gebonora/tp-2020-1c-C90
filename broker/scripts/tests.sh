74456d2b6266e92776b8b01f0b5f9e1247b2b6e9
Sebastian Gilberto <sebastiananibalgilberto@gmail.com>
2020-07-23 13:42:11 -0300

    Fixes in script

--- broker/scripts/tests.sh
+++ broker/scripts/tests.sh
@@ -13,8 +13,6 @@ LOGS_PATH=${BROKER_PATH}/tests/logs
 DUMPS_PATH=${BROKER_PATH}/tests/dumps
 SCRIPTS_PATH=${BROKER_PATH}/scripts
 DUMP_SCRIPT=${SCRIPTS_PATH}/dump.sh
-ERROR_MESSAGE=""
-ERROR=false
 
 declare -a tests=(
                 "buddy_fifo:buddy_basico"
@@ -26,64 +24,69 @@ declare -a tests=(
                 )
 
 
-echo "Backing up broker current config file: ${BROKER_CONFIG_FILE}"
-#cp ${BROKER_CONFIG_FILE} ${BROKER_BACKUP_FILE}
+echo -e "\nBacking up broker current config file: ${BROKER_CONFIG_FILE}"
+cp ${BROKER_CONFIG_FILE} ${BROKER_BACKUP_FILE}
 
 
 for test in "${tests[@]}" ; do
     TEST_NAME="${test%%:*}"
-    SCRIPT_TO_EXECUTE="${test##*:}"
+    SCRIPT_NAME="${test##*:}"
+    SCRIPT_TO_EXECUTE=${SCRIPTS_PATH}/${SCRIPT_NAME}.sh
     TEST_DUMP=${DUMPS_PATH}/${TEST_NAME}_dump.log
+    TEST_LOG=${LOGS_PATH}/${TEST_NAME}.log
+    TEST_CONFIG=${CONFIGS_PATH}/${TEST_NAME}.config
 
-    echo "Starting test: ${YELLOW} ${TEST_NAME} ${NC}"
+    echo "======================================================================" 
+
+    echo -e "\nStarting test: ${YELLOW} ${TEST_NAME} ${NC}"
 
     # copio el archivo de config del test, reemplazando el actual del broker
 
-    echo "Replacing broker config file ${YELLOW} ${BROKER_CONFIG_FILE} ${NC} with test configuration ${YELLOW} ${CONFIGS_PATH}/${TEST_NAME}.config ${NC}"
+    echo -e "\nReplacing broker config file ${YELLOW} ${BROKER_CONFIG_FILE} ${NC} with test configuration ${YELLOW} ${TEST_CONFIG} ${NC}"
 
-    #cp -r ${CONFIGS_PATH}/${TEST_NAME}.config ${BROKER_CONFIG_FILE}
+    cp -r ${CONFIGS_PATH}/${TEST_NAME}.config ${BROKER_CONFIG_FILE}
 
-    echo "Actual configuration is: "
+    echo -e "\nActual configuration is: "
 
     cat ${BROKER_CONFIG_FILE}
 
     # ejecuto la prueba correspondiente en script
 
-    echo "Executing script: ${YELLOW} ${SCRIPTS_PATH}/${SCRIPT_TO_EXECUTE}.sh ${NC}"
+    echo -e "\n\nExecuting script: ${YELLOW} ${SCRIPT_TO_EXECUTE} ${NC}"
 
-    #sh ${SCRIPTS_PATH}/${SCRIPT_TO_EXECUTE}.sh
+    sh ${SCRIPTS_PATH}/${SCRIPT_TO_EXECUTE}.sh
 
     # ejecuto dump de la memoria redireccionando el output a otro file
 
-    echo "Executing memory dump: ${YELLOW} ${DUMP_SCRIPT} ${NC}"
+    echo -e "\nExecuting memory dump: ${YELLOW} ${DUMP_SCRIPT} ${NC}"
 
-    #sh ${DUMP_SCRIPT} > ${TEST_DUMP}
+    sh ${DUMP_SCRIPT} > ${TEST_DUMP}
 
     # elimino la primera linea del archivo de dump copiado
 
-    echo "Deleting dump first line (timestamp)"
+    echo -e "\nDeleting dump first line (timestamp)"
 
-    #tail -n +2 "${TEST_DUMP}" > "${TEST_DUMP}.tmp" && mv "${TEST_DUMP}.tmp" "${TEST_DUMP}"
+    tail -n +2 "${TEST_DUMP}" > "${TEST_DUMP}.tmp" && mv "${TEST_DUMP}.tmp" "${TEST_DUMP}"
 
     # comparo el archivo de test contra el archivo de dump copiado
 
-    echo "Comparing dump ${YELLOW} ${TEST_DUMP} ${NC} with test log ${YELLOW} ${LOGS_PATH}/${TEST_NAME}.log ${NC}"
+    echo -e "\nComparing dump ${YELLOW} ${TEST_DUMP} ${NC} with test log ${YELLOW} ${TEST_LOG} ${NC}"
 
-    if [ cmp --silent ${TEST_DUMP} ${LOGS_PATH}/${TEST_NAME}.log ]; then
-        echo -e "${GREEN}Test ${TEST_NAME} passes${NC}"
+    if [ cmp --silent ${TEST_DUMP} ${TEST_LOG} ]; then
+        echo -e "Completed test ${YELLOW} ${TEST_NAME} ${NC} with status: ${GREEN} PASS ${NC}"
     else
-        echo -e "${RED}Test ${TEST_NAME} fails${NC}"
+        echo -e "Completed test ${YELLOW} ${TEST_NAME} ${NC} with status: ${RED} FAIL ${NC}"
     fi
 
-    echo "Completed test: ${TEST_NAME}"
+    sleep 2
 
 done
 
 # dejo el archivo de broker.config como estaba antes de los tests
 
-echo "Restoring broker config file from backup"
-#mv ${BROKER_BACKUP_FILE} ${BROKER_CONFIG_FILE}
+echo -e "\nRestoring broker config file from backup"
+mv ${BROKER_BACKUP_FILE} ${BROKER_CONFIG_FILE}
 
 #tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
 
-#cmp --silent $old $new || echo "files are different"
+#cmp --silent $old $new || echo -e "files are different"
