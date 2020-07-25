98b32ab2d9033840ae41dc88a9a5c3e2fc4c2f57
Sebastian Gilberto <sebastiananibalgilberto@gmail.com>
2020-07-25 15:36:19 -0300

    Funcion para obtener mejor entrenador

---
+++ team/scripts/prueba_final_team.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd ../../gameboy/deploy
+
+./gameboy TEAM APPEARED_POKEMON Pikachu 9 9
+./gameboy TEAM APPEARED_POKEMON Squirtle 6 7
+./gameboy TEAM APPEARED_POKEMON Onix 2 8
+./gameboy TEAM APPEARED_POKEMON Gengar 6 6
+./gameboy TEAM APPEARED_POKEMON Squirtle 5 5
--- team/src/App.c
+++ team/src/App.c
@@ -76,6 +76,7 @@ void warmUp() {
 	sem_init(&semaforoPokemone, 0, 0);
 	sem_init(&semaforoReady, 0, 0);
 	sem_init(&semaforoTrabajar2, 0, 0);
+	pthread_mutex_init(&messi,NULL);
 	pthread_mutex_init(&mtxBlock,NULL);
 	pthread_mutex_init(&mtxExec,NULL);
 	pthread_mutex_init(&mtxExit,NULL);
