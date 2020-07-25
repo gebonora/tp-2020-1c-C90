f9a8174b0791a4af3a006ef4c709cf2ca95ea0f6
Alan Zhao <alanzhag@hotmail.com>
2020-07-25 05:59:37 -0300

    Memleak fix.

--- team/src/manejadorDeEventos/ManejadorDeEventos.c
+++ team/src/manejadorDeEventos/ManejadorDeEventos.c
@@ -131,7 +131,9 @@ static void procesarCaughtRecibido(ManejadorDeEventos* this, Caught* unCaught, u
 static void destruir(ManejadorDeEventos * this) {
 	log_destroy(this->logger);
 	list_destroy_and_destroy_elements(this->listaLocalizedAppearedsRecibidos, free);
-	list_destroy(this->listaPokemonsNecesarios);
+	if (this->listaPokemonsNecesarios != NULL) {
+        list_destroy(this->listaPokemonsNecesarios);
+    }
 	free(this);
 }
 
@@ -150,7 +152,7 @@ static ManejadorDeEventos* new(ServicioDeCaptura* servicioDeCaptura, Registrador
 	manejador->registradorDeEventos = registradorDeEventos;
 	manejador->listaLocalizedAppearedsRecibidos = list_create();
 	manejador->servicioDeCaptura = servicioDeCaptura;
-	manejador->listaPokemonsNecesarios = list_create();
+	manejador->listaPokemonsNecesarios = NULL;
 	manejador->registrarpokemonsNecesarios  = &registrarpokemonsNecesarios;
 	manejador->procesarLocalizedRecibido = &procesarLocalizedRecibido;
 	manejador->procesarAppearedRecibido = &procesarAppearedRecibido;
