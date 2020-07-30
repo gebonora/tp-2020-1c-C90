4e8e54358447936d0b18985f5e7d26b23909c005
Gonzalo <gebonora@gmail.com>
2020-07-29 22:29:59 -0300

    Fix leaks, sockets not closing and recv falgs on Gameboy. Fix sockets not closing on Gamecard. Custom test scripts added for Gamecard.

---
+++ gameboy/custom_scripts/c_custom_get_pikachus_150_after.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd ../deploy
+
+./gameboy GAMECARD GET_POKEMON pikachu0  100
+./gameboy GAMECARD GET_POKEMON pikachu1  101
+./gameboy GAMECARD GET_POKEMON pikachu2  102
+./gameboy GAMECARD GET_POKEMON pikachu3  103
+./gameboy GAMECARD GET_POKEMON pikachu4  104
+./gameboy GAMECARD GET_POKEMON pikachu5  105
+./gameboy GAMECARD GET_POKEMON pikachu6  106
+./gameboy GAMECARD GET_POKEMON pikachu7  107
+./gameboy GAMECARD GET_POKEMON pikachu8  108
+./gameboy GAMECARD GET_POKEMON pikachu9  109
--- gameboy/src/App.c
+++ gameboy/src/App.c
@@ -1,50 +1,52 @@
 #include "app/App.h"
 
 int main(int argc, char **argv) {
-    INTERNAL_LOGGER = log_create(GAMEBOY_INTERNAL_LOG_FILE, "GameBoy.app", 1, LOG_LEVEL_DEBUG);
-    mostrarTitulo(INTERNAL_LOGGER);
-    log_info(INTERNAL_LOGGER, "========================= Inicio de ejecución ============================");
+	INTERNAL_LOGGER = log_create(GAMEBOY_INTERNAL_LOG_FILE, "GameBoy.app", 1, LOG_LEVEL_DEBUG);
+	mostrarTitulo(INTERNAL_LOGGER);
+	log_info(INTERNAL_LOGGER, "========================= Inicio de ejecución ============================");
 
-    // Config
-    log_debug(INTERNAL_LOGGER, "Levantando configuracion...");
-    servicioDeConfiguracion = ServicioDeConfiguracionConstructor.new(GAMEBOY_CONFIG_FILE, GAMEBOY_INTERNAL_LOG_FILE);
+	// Config
+	log_debug(INTERNAL_LOGGER, "Levantando configuracion...");
+	servicioDeConfiguracion = ServicioDeConfiguracionConstructor.new(GAMEBOY_CONFIG_FILE, GAMEBOY_INTERNAL_LOG_FILE);
 
-    // Logger obligatorio
-    log_debug(INTERNAL_LOGGER, "Configurando logger obligatorio...");
-    char * mandatoryLogPath = servicioDeConfiguracion.obtenerString(&servicioDeConfiguracion, MANDATORY_LOG);
-    MANDATORY_LOGGER = log_create(mandatoryLogPath, "LogObligatorio", 1, LOG_LEVEL_DEBUG);
+	// Logger obligatorio
+	log_debug(INTERNAL_LOGGER, "Configurando logger obligatorio...");
+	char * mandatoryLogPath = servicioDeConfiguracion.obtenerString(&servicioDeConfiguracion, MANDATORY_LOG);
+	MANDATORY_LOGGER = log_create(mandatoryLogPath, "LogObligatorio", 1, LOG_LEVEL_DEBUG);
 
-    // Controlador
-    if (argc > 1) {
-        ControladorPedidosGameBoy controladorPedidosGameBoy = ControladorPedidosGameBoyConstructor.new();
-        PedidoGameBoy pedidoGameBoy = crearPedidoGameBoy(argc, argv);
-        controladorPedidosGameBoy.despacharPedido(&controladorPedidosGameBoy, pedidoGameBoy);
-    } else {
-        log_error(INTERNAL_LOGGER, "No se ingresaron argumentos al proceso.");
-    }
+	// Controlador
+	if (argc > 1) {
+		ControladorPedidosGameBoy controladorPedidosGameBoy = ControladorPedidosGameBoyConstructor.new();
+		PedidoGameBoy pedidoGameBoy = crearPedidoGameBoy(argc, argv);
+		controladorPedidosGameBoy.despacharPedido(&controladorPedidosGameBoy, pedidoGameBoy);
+		controladorPedidosGameBoy.destruir(&controladorPedidosGameBoy);
+		list_destroy(pedidoGameBoy.argumentos);
+	} else {
+		log_error(INTERNAL_LOGGER, "No se ingresaron argumentos al proceso.");
+	}
 
-    // Liberacion
-    log_debug(INTERNAL_LOGGER, "Finalizando proceso GameBoy...");
-    log_debug(INTERNAL_LOGGER, "Liberando logger obligatorio");
-    log_destroy(MANDATORY_LOGGER);
-    log_debug(INTERNAL_LOGGER, "Liberando servicios");
-    servicioDeConfiguracion.destruir(&servicioDeConfiguracion);
-    log_debug(INTERNAL_LOGGER, "Saliendo...");
-    log_info(INTERNAL_LOGGER, "============================ Fin de ejecución ============================");
+	// Liberacion
+	log_debug(INTERNAL_LOGGER, "Finalizando proceso GameBoy...");
+	log_debug(INTERNAL_LOGGER, "Liberando logger obligatorio");
+	log_destroy(MANDATORY_LOGGER);
+	log_debug(INTERNAL_LOGGER, "Liberando servicios");
+	servicioDeConfiguracion.destruir(&servicioDeConfiguracion);
+	log_debug(INTERNAL_LOGGER, "Saliendo...");
+	log_info(INTERNAL_LOGGER, "============================ Fin de ejecución ============================");
 
-    log_destroy(INTERNAL_LOGGER);
-    return 0;
+	log_destroy(INTERNAL_LOGGER);
+	return 0;
 }
 
 void mostrarTitulo(t_log * logger) {
-    char *title = "\n"
-                  "==============================================================\n"
-                  " ██████   █████  ███    ███ ███████ ██████   ██████  ██    ██ \n"
-                  "██       ██   ██ ████  ████ ██      ██   ██ ██    ██  ██  ██  \n"
-                  "██   ███ ███████ ██ ████ ██ █████   ██████  ██    ██   ████   \n"
-                  "██    ██ ██   ██ ██  ██  ██ ██      ██   ██ ██    ██    ██    \n"
-                  " ██████  ██   ██ ██      ██ ███████ ██████   ██████     ██    \n"
-                  "==============================================================";
+	char *title = "\n"
+			"==============================================================\n"
+			" ██████   █████  ███    ███ ███████ ██████   ██████  ██    ██ \n"
+			"██       ██   ██ ████  ████ ██      ██   ██ ██    ██  ██  ██  \n"
+			"██   ███ ███████ ██ ████ ██ █████   ██████  ██    ██   ████   \n"
+			"██    ██ ██   ██ ██  ██  ██ ██      ██   ██ ██    ██    ██    \n"
+			" ██████  ██   ██ ██      ██ ███████ ██████   ██████     ██    \n"
+			"==============================================================";
 
-    log_info(logger, title);
+	log_info(logger, title);
 }
